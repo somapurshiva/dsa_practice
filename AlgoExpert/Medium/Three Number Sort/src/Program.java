3986b6b5c129f7ce0e5b1db4a91aaf572aa2c821
Shiva Somapur <ssomapur@vmware.com>
2021-12-09 12:02:38 +0530

    updated the three number sort logic with right algo

--- AlgoExpert/Medium/Three Number Sort/src/Program.java
+++ AlgoExpert/Medium/Three Number Sort/src/Program.java
@@ -3,11 +3,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Program {
-    public int[] threeNumberSort(int[] array, int[] order) {
+    public int[] threeNumberSortHashMap(int[] array, int[] order) {
         Map<Integer, Integer> countMap = new HashMap<>();
-        for (int i = 0; i < array.length; i++) {
-            countMap.putIfAbsent(array[i], 0);
-            countMap.put(array[i], countMap.get(array[i]) + 1);
+        for (int j : array) {
+            countMap.putIfAbsent(j, 0);
+            countMap.put(j, countMap.get(j) + 1);
         }
         int idx = 0;
         for (int key : order) {
@@ -16,6 +16,29 @@ public class Program {
                 array[idx++] = key;
         }
         return array;
+    }
+
+    public int[] threeNumberSort(int[] array, int[] order) {
+        int firstIdx = 0, secondIdx = 0, thirdIdx = array.length - 1;
+        while(secondIdx <= thirdIdx) {
+            if (array[secondIdx] == order[0]) {
+                swap(array, secondIdx, firstIdx);
+                firstIdx++;
+                secondIdx++;
+            } else if (array[secondIdx] == order[1])
+                secondIdx++;
+            else {
+                swap(array, thirdIdx, secondIdx);
+                thirdIdx--;
+            }
+        }
+        return array;
+    }
+
+    private void swap(int[] array, int idxA, int idxB) {
+        int temp = array[idxA];
+        array[idxA] = array[idxB];
+        array[idxB] = temp;
     }
 
     public static void main(String[] args) {
