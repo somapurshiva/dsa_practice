d090bf5d405dcc029493b671670c9843a17e1ad2
Shiva Somapur <ssomapur@vmware.com>
2022-01-24 12:11:24 +0530

    LC - Blind 75 --> 91. Decode Ways

--- LeetCode/Blind 75/91. Decode Ways/src/Solution.java
+++ LeetCode/Blind 75/91. Decode Ways/src/Solution.java
@@ -5,7 +5,9 @@ class Solution {
     Map<Integer, Integer> memo = new HashMap<>();
 
     public int numDecodings(String s) {
-        return recursiveWithMemo(0, s);
+//        return recursiveWithMemo(0, s);
+//        return dpIterativeWithSpace(s);
+        return dpIterativeConstSpace(s);
     }
 
     private int recursiveWithMemo(int index, String s) {
@@ -28,6 +30,46 @@ class Solution {
 
         memo.put(index, ans);
         return ans;
+    }
+
+    private int dpIterativeWithSpace(String s) {
+        if (s.charAt(0) == '0')
+            return 0;
+
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != '0')
+                dp[i + 1] = dp[i];
+
+            int twoDigit = Integer.parseInt(s.substring(i-1, i+1));
+            if (twoDigit >= 10 && twoDigit <= 26)
+                dp[i + 1] += dp[i-1];
+        }
+        return dp[s.length()];
+    }
+
+    private int dpIterativeConstSpace(String s) {
+        if (s.charAt(0) == '0')
+            return 0;
+        int twoBack = 1;
+        int oneBack = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            int current = 0;
+            if (s.charAt(i) != '0')
+                current += oneBack;
+
+            int twoDigit = Integer.parseInt(s.substring(i-1, i+1));
+            if (twoDigit >= 10 && twoDigit <= 26)
+                current += twoBack;
+
+            twoBack = oneBack;
+            oneBack = current;
+        }
+        return oneBack;
     }
 
     public static void main(String[] args) {
